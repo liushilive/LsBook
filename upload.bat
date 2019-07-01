5c2c3041d25a42514bb7d4af1740c51505e052bd
刘士 <liushilive@outlook.com>
2019-07-01 16:23:09 +0800

    复制最后加入 换行符

--- upload.bat
+++ upload.bat
@@ -1,6 +1,11 @@
 rem python setup.py sdist upload
 python setup.py sdist bdist_wheel
 
-twine upload dist/*
+twine upload dist/* --verbose
 
 pause
+
+rmdir /S /Q build
+rmdir /S /Q dist
+rmdir /S /Q LsBook.egg-info
+
