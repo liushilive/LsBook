4bb28c776a4df3fda35e13488a13c31c95732376
刘士 <liushilive@outlook.com>
2019-10-10 15:37:26 +0800

    修改资源

--- LsBook/assets/lsbook/lsbook.js
+++ LsBook/assets/lsbook/lsbook.js
@@ -3333,7 +3333,7 @@ function search() {
           url: page,
           title: store.title,
           body: store.body.substr(Math.max(0, index - 50), MAX_DESCRIPTION_SIZE)
-            // .replace(/^[^\s,.]+./, '').replace(/(..*)[\s,.].*/, '$1') // prevent break word
+          // .replace(/^[^\s,.]+./, '').replace(/(..*)[\s,.].*/, '$1') // prevent break word
             .replace(keywordRe, '<span class="search-highlight-keyword">$1</span>')
         });
       }
@@ -3547,6 +3547,11 @@ function fontsettings() {
       config: 'ant',
       text: 'Ant',
       id: 2
+    },
+    {
+      config: 'ym',
+      text: 'YM',
+      id: 3
     }
   ];
 
